cf992acac3b3929d7e2e11856e31f2ed28071b4d
Nikita Ekert <ekertn7@gmail.com>
2025-02-03 12:18:04 +0300

    Recursive sample

--- split_msg.py
+++ split_msg.py
@@ -2,62 +2,17 @@
 """Script to split html"""
 
 from typing import Generator
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 from config import DEFAULT_MAX_LEN, DEFAULT_FILE_PATH
 from argument_parser.argument_parser import create_argument_parser
 
 
-def split_message(source: str, max_len: int):  # -> Generator[str]:
-    """Splits the original message `source` into fragments of the specified
-    length `max_len`
-    """
-    html = BeautifulSoup(source, 'html.parser')
-    accumulated_elements = ''
-
-    for selected_element in html.find_all():
-        if len(str(accumulated_elements)) + len(str(selected_element)) <= max_len:
-            accumulated_elements += str(selected_element)
-        else:
-            print(accumulated_elements)
-            if len(str(selected_element)) <= max_len:
-                accumulated_elements = str(selected_element)
-            else:
-                print('--OOOOPS--')
-                print(selected_element)
-            print('\n--------\n')
-
-    # start = 0
-    # end = max_len
-
-    # while True:
-    #     if start >= len(source):
-    #         break
-    #     chunk = source[start:end]
-    #     yield chunk
-    #     start += max_len
-    #     end += max_len
-
-
-def search(source: str, close_tags: str = '', max_len: int = DEFAULT_MAX_LEN):
-    # базовый случай = вернет массив (когда длина (текущего элемента + предыдущего + закрывающих тегов) < max_len)
-    # рекурсивный случай = уйдет в рекурсию (строка + закрывающий тег)
-    all_items = BeautifulSoup(source, 'html.parser')
-
-    try:
-        extracted_item = all_items.find_all()[0].extract()
-    except IndexError:
-        return ''
-
-    if len(str(extracted_item)) + accumulated_item <= max_len:
-
-    # if len(accumulator) + len(str(extracted_tag)) <= max_len:
-    #     accumulator += str(extracted_tag)
-    #     return accumulator
-    #     # return search(item=tags, accumulator=accumulator)
-
-    # if len(accumulator) + len(str(extracted_tag)) > max_len:
-    #     return search(item=tags, accumulator='')
+def search(source: list[Tag]) -> list[Tag]:
+    if sum(len(i) for i in source) >= max_len:
+        return search(source[0]) + search(source[1:])
+    else:
+        return source
 
 
 if __name__ == "__main__":
@@ -70,16 +25,8 @@ if __name__ == "__main__":
     with open(file_path, 'r', encoding='utf-8') as file:
         source = file.read()
 
-    split_message(source=source, max_len=max_len)
+    html = BeautifulSoup(source, 'html.parser').find_all()
 
-    # for item in search(source=source):
-    #     print(item)
-    #     print('----------\n')
+    print(search(source=html))
 
-    print(search(source=source))
-
-    # for item in split_message(source=source, max_len=max_len):
-    #     print(item)
-    #     print()
-    #     print('----------------------')
-    #     print()
+    # html.descendant
